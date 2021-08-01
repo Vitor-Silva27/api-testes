e9de53e8c0ee0f883485836c500f8da4f303d2e7
Vitor Silva <developer.vitorsilva@gmail.com>
2021-08-01 17:26:50 -0300

    created test to not save an user that already exists

--- src/modules/createUser/CreateUserService.spec.ts
+++ src/modules/createUser/CreateUserService.spec.ts
@@ -1,12 +1,19 @@
 import { User } from "../../entities/User";
 import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
+import { IUsersRepository } from "../../repositories/IUsersRepositories";
 import { CreateUserService } from "./CreateUserService";
 
 describe("Create user", () => {
 
+    let usersRepository: IUsersRepository;
+    let createUserService: CreateUserService;
+
+    beforeAll(() => {
+        usersRepository = new UsersRepositoryInMemory();
+        createUserService = new CreateUserService(usersRepository);
+    })
+
     it('should be able to create a new user', async () => {
-        const usersRepository = new UsersRepositoryInMemory();
-        const createUserService = new CreateUserService(usersRepository);
         const userData:User = {
             name: "Test Name",
             email: "test@test.com",
@@ -16,5 +23,19 @@ describe("Create user", () => {
         
         expect(user).toHaveProperty("id");
     });
+
+    it("should not be able to create an existing user", async () => {
+        const userData: User = {
+          name: "Test Existing Name",
+          email: "testexisting@test.com.br",
+          username: "testexisting",
+        };
+    
+        await createUserService.execute(userData);
+    
+        await expect(createUserService.execute(userData)).rejects.toEqual(
+          new Error("User already exists!")
+        );
+      });
 
 });
